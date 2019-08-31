b4d277acd9d3bddf71d01b6ae7cd1daff98daed2
Christoph Lienhard <Christoph.Lienhard@tum.de>
2019-08-31 17:01:42 +0200

    Add tests for backend

--- backend/dev-setup.sh
+++ backend/dev-setup.sh
@@ -5,3 +5,4 @@ python -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
+pip install -r requirements_dev.txt
---
+++ backend/flask-server/tests/test_main.py
@@ -0,0 +1,45 @@
+import json
+
+from main import kandidaten, fragen, kategorien
+
+
+def test_kandidaten():
+    expected_result = [
+        {
+            "id": 0,
+            "vorname": "Max",
+            "name": "Mustermann",
+            "email": "max.mustermann@yahoo.com"
+        },
+        {
+            "id": 1,
+            "vorname": "Erika",
+            "name": "Mustermann",
+            "email": "erika.mustermann@yahoo.com"
+        }
+    ]
+    assert json.loads(kandidaten()) == expected_result
+
+
+def test_fragen():
+    expected_result = [
+        {
+            "id": 0,
+            "text": "Dies ist eine Dummy Frage für Testzwecke",
+            "kategorie_id": 0
+        }
+    ]
+
+    assert json.loads(fragen()) == expected_result
+
+
+def test_kategorien():
+    expected_result = [{
+        "id": 0,
+        "name": "Umwelt"
+    }, {
+        "id": 1,
+        "name": "Soziales"
+    }]
+
+    assert json.loads(kategorien()) == expected_result
