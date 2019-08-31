d86e9d0d6f2c9cec46ddfa4e0e8ea927d311e6ef
Pit-Storm <peter@einhalbblindgaenger.de>
2019-08-31 22:25:45 +0200

    added request query kat_id for /fragen

--- backend/flask-server/main.py
+++ backend/flask-server/main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+import json
 
 app = Flask(__name__)
 
@@ -8,10 +10,22 @@ def root():
     return "Candymat Data Backend"
 
 
-@app.route('/fragen')
+@app.route('/fragen/')
 def fragen():
-    with open('data/fragen.json', 'r') as json_file:
-        return json_file.read()
+    with open('data/fragen.json', 'r', encoding="utf-8") as json_file:
+        fragen = json.load(json_file)
+        
+    # Get the url Request Parameter "kat_id"
+    kat_id = request.args.get('kat_id')
+    
+    if kat_id == None:
+        return json.dumps(fragen)
+    else:
+        answer = []
+        for frage in fragen:
+            if json.dumps(frage["kategorie_id"] )== kat_id:
+                answer.append(frage)
+        return json.dumps(answer)
 
 
 @app.route('/kandidaten')
